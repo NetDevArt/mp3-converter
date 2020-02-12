6e97f17441c9b1a8c68b0f70c35846f17ec67424
TOUR-ARTHUR\Arthur <145484@supinfo.com>
2020-02-12 21:42:31 +0100

    Add a constant to tell the programm if you want to specify location of your MediaInfo dll. Then, correcting the Audio track detection by the 'track_type' media info. In fact, some files had no bit_rate information whereas there is audio track.

--- mp3_converter.py
+++ mp3_converter.py
@@ -5,6 +5,8 @@ from modules.pymediainfo.pymediainfo import MediaInfo
 import tkinter as tk
 from tkinter import filedialog
 
+ASK_DLL_LOCATION = False
+
 
 def ask_directory(window_title):
     """
@@ -21,28 +23,33 @@ def ask_file(window_title):
     """
     root = tk.Tk()
     root.withdraw()
-    return filedialog.askopenfile(title=window_title)
+    media_info_path = os.path.join(os.path.realpath(__file__))
+    if ASK_DLL_LOCATION:
+        return filedialog.askopenfile(title=window_title).name
+    else:
+        return os.path.join(os.path.dirname(__file__), 'modules', 'pymediainfo', 'pymediainfo', 'MediaInfo.dll')
 
 
 def list_files_to_convert():
     """
     Generate a hot file list (with its path)
+    Get all files with a bit_rate information, which confirm that it is a
     yield return a result and forget it (less memory eating)
     """
-    for root, dirs, files in os.walk(video_dir, topdown=False):
+    for root, dirs, files in os.walk(video_dir):
         file_list = [name for name in files if not name.endswith('.mp3')]
         for name in file_list:
             filepath = os.path.join(root, name)
             media_info = MediaInfo.parse(filepath, library_file=dll_path)
             for track in media_info.tracks:
-                if track.bit_rate is not None:
+                if 'Audio' in track.track_type:
                     # print(track.track_type, track.bit_rate)
                     # print(filepath, "Is an Audio/Video file, and should be converted")
                     yield dict(path=filepath, info=media_info)
 
 
 # Path where the MediaInfo.dll is located
-dll_path = ask_file("Select the MediaInfo.dll").name
+dll_path = ask_file("Select the MediaInfo.dll location")
 # Path where the videos are located
 video_dir = ask_directory("Select your music folder to be processed recursively")
 
@@ -52,7 +59,7 @@ my_list = list_files_to_convert()
 for file in my_list:
     path = file['path']
     info = file['info']
-    if os.path.exists(path) and not path.endswith('.mp3'):
+    if os.path.exists(path):
         file_dir = info.tracks[0].folder_name
         filename = info.tracks[0].file_name
         mp3_file_dir = os.path.join(file_dir, 'mp3')
@@ -62,7 +69,7 @@ for file in my_list:
         mp3_converted_filepath = os.path.join(mp3_file_dir, mp3_file_name)
         if not os.path.isfile(mp3_converted_filepath):
             try:
-                # Subprocess the conversion with ffmpeg, don't overwrite if exists
+                # Subprocess the conversion with ffmpeg, don't overwrite if exists with '-n' argument
                 subprocess.check_call(['ffmpeg', '-i', path, '-vcodec', 'copy', '-codec:a', 'libmp3lame', mp3_converted_filepath, '-n'])
             except subprocess.CalledProcessError as e:
                 print("ERROR: ", e.output)
